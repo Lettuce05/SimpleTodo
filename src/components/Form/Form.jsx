ab59b8a979e161367d15970365ee2b9ac1ec7be2
Anthony Romaine <anthony.romaine1@gmail.com>
2022-10-23 01:23:45 +0000

    Initial commit

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import './Form.css';
+
+export default function Form() {
+  return (
+    <form>
+      <div className='form'>
+        <input className='form__input' type='text' placeholder='Write a new task' />
+        <button className='btn-submit'>+</button>
+      </div>
+    </form>
+  )
+}
