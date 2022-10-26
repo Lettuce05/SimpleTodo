5bee075ab3f6605a4bfc21bd1ed089218853c3e1
Anthony Romaine <anthony.romaine1@gmail.com>
2022-10-26 06:30:25 +0000

    Install uuid

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,6 +1,7 @@
 import create from 'zustand';
+import { v4 as uuidv4 } from 'uuid';
 
 export const todoSlice = create((set) => ({
   todos: [],
-  addTodo: (text) => set((state) => ({ todos: [...state.todos, {text: text}]})),
+  addTodo: (text) => set((state) => ({ todos: [...state.todos, { text: text, id: uuidv4(), completed: false }] })),
 }));
